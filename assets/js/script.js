622db335e839d666e4902216218015253263e9fc
ANDO Andriamalala <integrateur@rova-caviar.com>
2022-01-25 13:47:45 +0300

    animated home header

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// animated header background
+
+new FinisherHeader({
+    "count": 100,
+    "size": {
+      "min": 2,
+      "max": 8,
+      "pulse": 0
+    },
+    "speed": {
+      "x": {
+        "min": 0,
+        "max": 0.4
+      },
+      "y": {
+        "min": 0,
+        "max": 0.6
+      }
+    },
+    "colors": {
+      "background": "#201e30",
+      "particles": [
+        "#ffffff"
+      ]
+    },
+    "blending": "overlay",
+    "opacity": {
+      "center": 1,
+      "edge": 0
+    },
+    // "skew": -2,
+    "shapes": [
+      "c"
+    ]
+});
