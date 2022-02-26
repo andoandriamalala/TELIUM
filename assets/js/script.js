be4c72d671577eec9d9685e917b2eeb0e91aef23
ANDO Andriamalala <andriamalala5056@gmail.com>
2022-02-26 06:30:27 +0300

    slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -62,143 +62,143 @@ moveImg();
 
 
 // smooth scroll
-const math = {
-  lerp: (a, b, n) => {
-    return (1 - n) * a + n * b;
-  },
-  norm: (value, min, max) => {
-    return (value - min) / (max - min);
-  }
-};
+// const math = {
+//   lerp: (a, b, n) => {
+//     return (1 - n) * a + n * b;
+//   },
+//   norm: (value, min, max) => {
+//     return (value - min) / (max - min);
+//   }
+// };
 
-const config = {
-  height: window.innerHeight,
-  width: window.innerWidth
-};
+// const config = {
+//   height: window.innerHeight,
+//   width: window.innerWidth
+// };
 
-class Smooth {
-  constructor() {
-    this.bindMethods();
+// class Smooth {
+//   constructor() {
+//     this.bindMethods();
 
-    this.data = {
-      ease: 0.1,
-      current: 0,
-      last: 0,
-      rounded: 0
-    };
+//     this.data = {
+//       ease: 0.1,
+//       current: 0,
+//       last: 0,
+//       rounded: 0
+//     };
 
-    this.dom = {
-      el: document.querySelector("[data-scroll]"),
-      content: document.querySelector("[data-scroll-content]")
-    };
+//     this.dom = {
+//       el: document.querySelector("[data-scroll]"),
+//       content: document.querySelector("[data-scroll-content]")
+//     };
 
-    this.rAF = null;
+//     this.rAF = null;
 
-    this.init();
-  }
+//     this.init();
+//   }
 
-  bindMethods() {
-    ["scroll", "run", "resize"].forEach(
-      (fn) => (this[fn] = this[fn].bind(this))
-    );
-  }
+//   bindMethods() {
+//     ["scroll", "run", "resize"].forEach(
+//       (fn) => (this[fn] = this[fn].bind(this))
+//     );
+//   }
 
-  setStyles() {
-    Object.assign(this.dom.el.style, {
-      position: "fixed",
-      top: 0,
-      left: 0,
-      height: "100%",
-      width: "100%",
-      overflow: "hidden"
-    });
-  }
+//   setStyles() {
+//     Object.assign(this.dom.el.style, {
+//       position: "fixed",
+//       top: 0,
+//       left: 0,
+//       height: "100%",
+//       width: "100%",
+//       overflow: "hidden"
+//     });
+//   }
 
-  setHeight() {
-    document.body.style.height = `${
-      this.dom.content.getBoundingClientRect().height
-    }px`;
-  }
+//   setHeight() {
+//     document.body.style.height = `${
+//       this.dom.content.getBoundingClientRect().height
+//     }px`;
+//   }
 
-  resize() {
-    this.setHeight();
-    this.scroll();
-  }
+//   resize() {
+//     this.setHeight();
+//     this.scroll();
+//   }
 
-  preload() {
-    imagesLoaded(this.dom.content, (instance) => {
-      this.setHeight();
-    });
-  }
+//   preload() {
+//     imagesLoaded(this.dom.content, (instance) => {
+//       this.setHeight();
+//     });
+//   }
 
-  scroll() {
-    this.data.current = window.scrollY;
-  }
+//   scroll() {
+//     this.data.current = window.scrollY;
+//   }
 
-  run() {
-    this.data.last += (this.data.current - this.data.last) * this.data.ease;
-    this.data.rounded = Math.round(this.data.last * 100) / 100;
+//   run() {
+//     this.data.last += (this.data.current - this.data.last) * this.data.ease;
+//     this.data.rounded = Math.round(this.data.last * 100) / 100;
 
-    const diff = this.data.current - this.data.rounded;
-    const acc = diff / config.width;
-    const velo = +acc;
-    const skew = velo * 7.5;
+//     const diff = this.data.current - this.data.rounded;
+//     const acc = diff / config.width;
+//     const velo = +acc;
+//     const skew = velo * 7.5;
 
-    this.dom.content.style.transform = `translate3d(0, -${this.data.rounded}px, 0) skewY(${skew}deg)`;
+//     this.dom.content.style.transform = `translate3d(0, -${this.data.rounded}px, 0) skewY(${skew}deg)`;
 
-    this.requestAnimationFrame();
-  }
+//     this.requestAnimationFrame();
+//   }
 
-  on() {
-    this.setStyles();
-    this.setHeight();
-    this.addEvents();
+//   on() {
+//     this.setStyles();
+//     this.setHeight();
+//     this.addEvents();
 
-    this.requestAnimationFrame();
-  }
+//     this.requestAnimationFrame();
+//   }
 
-  off() {
-    this.cancelAnimationFrame();
+//   off() {
+//     this.cancelAnimationFrame();
 
-    this.removeEvents();
-  }
+//     this.removeEvents();
+//   }
 
-  requestAnimationFrame() {
-    this.rAF = requestAnimationFrame(this.run);
-  }
+//   requestAnimationFrame() {
+//     this.rAF = requestAnimationFrame(this.run);
+//   }
 
-  cancelAnimationFrame() {
-    cancelAnimationFrame(this.rAF);
-  }
+//   cancelAnimationFrame() {
+//     cancelAnimationFrame(this.rAF);
+//   }
 
-  destroy() {
-    document.body.style.height = "";
+//   destroy() {
+//     document.body.style.height = "";
 
-    this.data = null;
+//     this.data = null;
 
-    this.removeEvents();
-    this.cancelAnimationFrame();
-  }
+//     this.removeEvents();
+//     this.cancelAnimationFrame();
+//   }
 
-  resize() {
-    this.setHeight();
-    this.data.rounded = this.data.last = this.data.current;
-  }
+//   resize() {
+//     this.setHeight();
+//     this.data.rounded = this.data.last = this.data.current;
+//   }
 
-  addEvents() {
-    window.addEventListener("resize", this.resize, { passive: true });
-    window.addEventListener("scroll", this.scroll, { passive: true });
-  }
+//   addEvents() {
+//     window.addEventListener("resize", this.resize, { passive: true });
+//     window.addEventListener("scroll", this.scroll, { passive: true });
+//   }
 
-  removeEvents() {
-    window.removeEventListener("resize", this.resize, { passive: true });
-    window.removeEventListener("scroll", this.scroll, { passive: true });
-  }
+//   removeEvents() {
+//     window.removeEventListener("resize", this.resize, { passive: true });
+//     window.removeEventListener("scroll", this.scroll, { passive: true });
+//   }
 
-  init() {
-    this.preload();
-    this.on();
-  }
-}
+//   init() {
+//     this.preload();
+//     this.on();
+//   }
+// }
 
-new Smooth();
+// new Smooth();
