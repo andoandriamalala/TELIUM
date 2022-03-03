64b8a43a35a55e55006d952afd6d9fda85a730fe
ANDO Andriamalala <andriamalala5056@gmail.com>
2022-03-03 22:04:52 +0300

    cta

--- assets/js/script.js
+++ assets/js/script.js
@@ -211,7 +211,7 @@ var i = 0;
 		var html = document.getElementById("text").innerHTML;
 		var attr = tag.setAttribute("data", html);
 		var txt = tag.getAttribute("data");
-		var speed = 170;
+		var speed = 200;
 
 		function typeWriter() {
 		  if (i <= txt.length) {
